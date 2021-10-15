20e3ee3d2b095fcb0b601b64d6dec3c107e5e8d3
larry-techs <52524118+larry-techs@users.noreply.github.com>
2021-10-15 16:44:01 +0300

    first commit

---
+++ Running pingu/Assets/Script/gh.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Input.GetKeyDown(KeyCode.A));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
